68bcc55901b31849f74ad2d75c23c1411d9e8d35
Fauno Guazina <prof.guazina@gmail.com>
2020-12-20 15:12:03 +0100

    corrections after testing

--- diceGame/src/main/java/com/dice_game/crud/utilities/exceptions/PlayerServImplException.java
+++ diceGame/src/main/java/com/dice_game/crud/utilities/exceptions/PlayerServImplException.java
@@ -4,10 +4,10 @@ public class PlayerServImplException extends RuntimeException {
 
 	private static final long serialVersionUID = 424786931058427752L;
 
-	private static final String DETAILS = "Exception type Player Service Implementation: ";
+	private static final String DETAILS = "Exception type Player Service Implementation";
 
 	public PlayerServImplException(String message) {
-		super(DETAILS.concat(message));
+		super(String.format("%s : %s!!!", DETAILS, message));
 	}
 	
 	public static void throwsUp(String message) {
--- diceGame/src/test/java/com/dice_game/crud/utilities/exceptions/PlayerServImplExceptionTest.java
+++ diceGame/src/test/java/com/dice_game/crud/utilities/exceptions/PlayerServImplExceptionTest.java
@@ -2,11 +2,13 @@ package com.dice_game.crud.utilities.exceptions;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class PlayerServImplExceptionTest {
 
 	@Test
+	@DisplayName("Exception is thrown")
 	void testThrowsUp() {
 		assertThrows(PlayerServImplException.class, () -> PlayerServImplException.throwsUp("teste"));
 	}
