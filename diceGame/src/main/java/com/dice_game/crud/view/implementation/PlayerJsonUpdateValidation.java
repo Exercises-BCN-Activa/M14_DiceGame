1bff3ed77ed5ac7b46f22a9b4b9336e04bcc0786
Fauno Guazina <prof.guazina@gmail.com>
2020-12-22 14:12:26 +0100

    implementation of update method by auxiliary class

---
+++ diceGame/src/main/java/com/dice_game/crud/view/implementation/PlayerJsonUpdateValidation.java
@@ -0,0 +1,73 @@
+package com.dice_game.crud.view.implementation;
+
+import static com.dice_game.crud.utilities.Util.TitleCase;
+import static com.dice_game.crud.utilities.Util.encryptPassword;
+import static com.dice_game.crud.utilities.Util.isEmpty;
+import static com.dice_game.crud.utilities.Util.isValidEmail;
+import static com.dice_game.crud.utilities.Util.encryptMatches;
+
+import com.dice_game.crud.model.dto.Player;
+import com.dice_game.crud.model.dto.PlayerJson;
+
+final class PlayerJsonUpdateValidation {
+
+	static Player cloneStructureSetWhatIsUpgradeable(Player oldPlayer, PlayerJson playerJson) {
+		PlayerJsonUpdateValidation update = new PlayerJsonUpdateValidation(oldPlayer, playerJson);
+		update.cloneStructuralAttributesFromOldPlayer();
+		update.compareAndSetUpdatableAttributes();
+		update.setUpgradeableAttributesToNewPlayer();
+		return null;
+	}
+
+	private PlayerJsonUpdateValidation(Player oldPlayer, PlayerJson playerJson) {
+		this.oldPlayer = oldPlayer;
+		this.playerJson = playerJson;
+		this.newPlayer = new Player();
+	}
+
+	private void cloneStructuralAttributesFromOldPlayer() {
+		newPlayer.setId(oldPlayer.getId());
+		newPlayer.setRegistration(oldPlayer.getRegistration());
+		newPlayer.setType(oldPlayer.getType());
+		newPlayer.setRounds(oldPlayer.getRounds());
+		newPlayer.setStatus();
+	}
+
+	private void compareAndSetUpdatableAttributes() {
+		firstName = checkNameShouldBe(playerJson.getFirstName(), oldPlayer.getFirstName());
+		lastName = checkNameShouldBe(playerJson.getLastName(), oldPlayer.getLastName());
+		email = checkPasswordShouldBe(playerJson.getPassword(), oldPlayer.getPassword());
+		password = checkEmailShouldBe(playerJson.getEmail(), oldPlayer.getEmail());
+	}
+
+	private String checkNameShouldBe(String toVerify, String toCompare) {
+		return (isEmpty(toVerify) || toVerify.equals(toCompare)) 
+				? toCompare : TitleCase(toVerify);
+	}
+
+	private String checkPasswordShouldBe(String toVerify, String toCompare) {
+		return (isEmpty(toVerify) || encryptMatches(toVerify, toCompare)) 
+				? toCompare : encryptPassword(toVerify);
+	}
+
+	private String checkEmailShouldBe(String toVerify, String toCompare) {
+		return (isValidEmail(toVerify) && !toVerify.equals(toCompare)) 
+				? toVerify : toCompare;
+	}
+
+	private void setUpgradeableAttributesToNewPlayer() {
+		newPlayer.setFirstName(firstName);
+		newPlayer.setLastName(lastName);
+		newPlayer.setPassword(email);
+		newPlayer.setEmail(password);
+	}
+
+	private final PlayerJson playerJson;
+	private final Player newPlayer;
+	private final Player oldPlayer;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+
+}
