0fbfca3df1cc1c9b11e0752f7111cb4713073c9a
Fauno Guazina <prof.guazina@gmail.com>
2020-12-31 11:54:50 +0100

    creation of utilitarian response class and its tests

---
+++ diceGame/src/main/java/com/dice_game/crud/utilities/Response.java
@@ -0,0 +1,53 @@
+package com.dice_game.crud.utilities;
+
+import java.util.HashMap;
+
+public class Response {
+	
+	public static Response error(String message) {
+		Response response = new Response();
+		response.put("success", false);
+		response.put("message", message);
+		return response;
+	}
+	
+	public static Response success(String message, Object content) {
+		Response response = new Response();
+		response.put("success", true);
+		response.put("message", message);
+		response.put("content", content);
+		return response;
+	}
+	
+	public String getMessage() {
+		return (String) get("message");
+	}
+	
+	public boolean isSuccess() {
+		return (boolean) get("success");
+	}
+	
+	public Object getContent() {
+		return (map.containsKey("content")) ? get("content") : CONTENT_ERROR;
+	}
+	
+	@Override
+	public String toString() {
+		return "Response : " + map;
+	}
+	
+	private Response() {
+		map = new HashMap<>();
+	}
+	
+	private void put(String key, Object value) {
+		map.put(key, value);
+	}
+	
+	private Object get(String key) {
+		return map.get(key);
+	}
+	
+	private final HashMap<String, Object> map;
+	private final String CONTENT_ERROR = "Error response have no content!";
+}
---
+++ diceGame/src/test/java/com/dice_game/crud/utilities/ResponseTest.java
@@ -0,0 +1,49 @@
+package com.dice_game.crud.utilities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.dice_game.crud.model.dto.PlayerJson;
+
+class ResponseTest {
+	
+	private Response response;
+	private String message;
+	private final String CONTENT_ERROR = "Error response have no content!";
+
+	@Test
+	@DisplayName("Error Response with Message")
+	void testResponseError() {
+		message = "Something went wrong trying to Creating Player";
+		response = Response.error(message);
+		
+		assertAll(
+				() -> assertNotNull(response, msgError("Not Null 1")),
+				() -> assertEquals(message, response.getMessage(), msgError("Equals 1")),
+				() -> assertEquals(CONTENT_ERROR, response.getContent(), msgError("Equals 2")),
+				() -> assertFalse(response.isSuccess(), msgError("False 1"))
+				);
+	}
+	
+	@Test
+	@DisplayName("Success Response with Message")
+	void testResponseSuccess() {
+		message = "Everything went well to Creating Player";
+		PlayerJson playerJson = new PlayerJson();
+		response = Response.success(message, playerJson);
+		
+		assertAll(
+				() -> assertNotNull(response, msgError("Not Null 1")),
+				() -> assertEquals(message, response.getMessage(), msgError("Equals 1")),
+				() -> assertEquals(playerJson, response.getContent(), msgError("Equals 2")),
+				() -> assertTrue(response.isSuccess(), msgError("True 1"))
+				);
+	}
+	
+	private String msgError(String input) {
+		return String.format("Error in Assert %s", input);
+	}
+
+}
