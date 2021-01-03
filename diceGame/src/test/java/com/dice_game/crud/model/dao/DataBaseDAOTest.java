0b07c919a36c3afbaab61e86415109ea32e698f0
Fauno Guazina <prof.guazina@gmail.com>
2021-01-03 10:11:51 +0100

    test creation that implements ADMIN in the database to manage application

--- diceGame/src/test/java/com/dice_game/crud/model/dao/DataBaseDAOTest.java
+++ diceGame/src/test/java/com/dice_game/crud/model/dao/DataBaseDAOTest.java
@@ -292,6 +292,37 @@ class DataBaseDAOTest {
 		
 		assertFalse(playerDAO.findByEmail(email).isPresent());
 	}
+	
+	@Test
+	@Order(16)
+	@Rollback(false)
+	@DisplayName("Create a new player in DB")
+	public void testCreateAdminToIniciateTheAplication() {
+		
+		String adminEmail = "admin@dicegame.com";
+
+		Player admin = new Player();
+		admin.setEmail(adminEmail);
+		admin.setPassword(Util.encryptPassword("root"));
+		admin.setFirstName("Admin");
+		admin.setLastName("Root");
+		admin.setType(Role.FULL);
+
+		Player adminSaved = (playerDAO.existsByEmail(adminEmail)) 
+				? playerDAO.findByEmail(adminEmail).get()
+				: playerDAO.save(admin);
+		
+		assertAll(
+				() -> assertEquals(admin.getEmail(), adminSaved.getEmail()),
+				() -> assertEquals(admin.getFirstName(), adminSaved.getFirstName()),
+				() -> assertEquals(admin.getLastName(), adminSaved.getLastName()),
+				() -> assertEquals(Role.FULL.getRole(), adminSaved.getType()),
+				() -> assertTrue(Util.encryptMatches("root", adminSaved.getPassword())),
+				() -> assertNotNull(adminSaved.getId()),
+				() -> assertNotNull(adminSaved.getRegistration())
+				);
+
+	}
 
 	private String msgError(String input) {
 		return String.format("%s should be equals because they were created from the same attribute", input);
