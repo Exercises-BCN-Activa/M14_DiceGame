fe15d2fe8bcb475e2111414710eb2aab0d4501c2
Fauno Guazina <prof.guazina@gmail.com>
2021-01-10 13:24:28 +0100

    sql basic admin

---
+++ diceGame/src/main/resources/data.sql
@@ -0,0 +1,20 @@
+
+
+INSERT INTO PLAYER
+(
+email,
+first_name,
+last_name,
+password,
+type
+) 
+VALUES 
+(
+'admin@dicegame.com',
+'Admin',
+'Root',
+'$2a$10$ARVQ4tCURhfnPwlL1IC3K.1YRlNLNtq4.cOcuLihSLM7y9qGg1JR.',
+'USER,ADMIN'
+);
+
+
